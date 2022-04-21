9c3c0989cc3efcc746cb3b55ba6c7fdb3c278375
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-04-21 16:03:27 +0600

    Favicon and Site name Changed

--- src/Components/About Me/About.js
+++ src/Components/About Me/About.js
@@ -2,8 +2,36 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="mt-24">
-      <h1 className="text-6xl">About Page</h1>
+    <div className="mt-32 justify-center">
+      <h1 className="text-3xl text-center text-blue-700">
+        🎯 My Goals in Next 4 Months 🎯
+      </h1>
+      <hr />
+      <img
+        className="w-[80%] mb-10 md:w-[30%] mx-auto mt-12"
+        src="https://sakib-first-assingment.netlify.app/images/personal/apic1.png"
+        alt=""
+      />
+      <p className="text-justify w-1/2 mx-auto text-xl text-cyan-400">
+        ❝Focus is a valuable ability that can help people complete their tasks
+        at work and promote productivity in their professional lives. Learning
+        about strategies for concentration can help you achieve your goals and
+        minimize distractions.❞
+      </p>
+      <p className="text-justify md:text-justify w-1/2 mx-auto">
+        Currently,I am Student of Premier University,Chittagong.I am Completing
+        my BSC Degree in Computer Science Here.As an Student I have pressures of
+        joining my Classes regularly and take part of Class Test,Quizes and
+        exams.That's why it's tough for me to Join anywhere as an Junior Web
+        Developer.Because If I Join Any Company I have to Spend Minimum 6-8
+        Hours there which is currently very tough for me!So,I have decided to
+        before I complete my undergraduation I must practise Javacript,React
+        JS,Node JS,MongoDB Regularly and Join Online Based Freelancing
+        marketplaces like - "Fiverr","UpWork" etc. to Become a Freelancer.I am
+        very Hopeful to Complete my Goals in next 4 months!'
+        <br />
+        <span className="text-xl text-indigo-700"> Thank You!</span>
+      </p>
     </div>
   );
 };
