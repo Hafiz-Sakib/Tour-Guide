353d5b51625564e2926d51c0dec3d6e5a11f58fb
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-04-22 09:23:09 +0600

    login form fixed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -17,20 +17,51 @@ const Login = () => {
             <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
               <form>
                 <div className="mb-6">
-                  <input
-                    type="text"
-                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    placeholder="Email address"
-                  />
+                  <div class="flex flex-col pt-4">
+                    <div class="flex relative ">
+                      <span class=" inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                        <svg
+                          width="15"
+                          height="15"
+                          fill="currentColor"
+                          viewBox="0 0 1792 1792"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"></path>
+                        </svg>
+                      </span>
+                      <input
+                        type="email"
+                        required
+                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        placeholder="Email address"
+                      />
+                    </div>
+                  </div>
                 </div>
                 <div className="mb-6">
-                  <input
-                    type="password"
-                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    placeholder="Password"
-                  />
+                  <div class="flex flex-col pt-4">
+                    <div class="flex relative ">
+                      <span class=" inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                        <svg
+                          width="15"
+                          height="15"
+                          fill="currentColor"
+                          viewBox="0 0 1792 1792"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"></path>
+                        </svg>
+                      </span>
+                      <input
+                        type="password"
+                        required
+                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        placeholder="Password"
+                      />
+                    </div>
+                  </div>
                 </div>
-
                 <div className="flex justify-between items-center mb-6">
                   <div className="form-group form-check">
                     <input
@@ -56,8 +87,6 @@ const Login = () => {
                 <button
                   type="submit"
                   className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
                 >
                   Sign in
                 </button>
@@ -102,7 +131,7 @@ const Login = () => {
                   </button>
                   <button
                     type="button"
-                    className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-7 py-3 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2 active:shadow-lg"
+                    className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-8 py-3 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2 active:shadow-lg"
                   >
                     <svg
                       className="w-4 h-4 mr-2 -ml-1"
