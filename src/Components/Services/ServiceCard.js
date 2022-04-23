ba2674c1565cb7e596ea9cd5616b992313cea45c
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-04-23 11:01:28 +0600

    Link added on Book Now

--- src/Components/Services/ServiceCard.js
+++ src/Components/Services/ServiceCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ServiceCard.css";
 
 const ServiceCard = (props) => {
@@ -20,12 +21,14 @@ const ServiceCard = (props) => {
             <p className="text-gray-700 text-base mb-4">{about}</p>
             <p className="text-red-600"> Cost: {balance}</p>
             <br />
-            <button
-              type="button"
-              className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-            >
-              Book Now!
-            </button>
+            <Link to={"/Login"}>
+              <button
+                type="button"
+                className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                Book Now!
+              </button>
+            </Link>
           </div>
         </div>
       </div>
