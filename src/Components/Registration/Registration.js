8fcc6abe2bc99441f3cd65df031c4ed88cf9789d
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-04-29 12:48:43 +0600

    Confirm password

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -53,17 +53,7 @@ const Registration = () => {
           <div className="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32">
             <p className="text-3xl text-center">Welcome.</p>
             <form className="flex flex-col pt-3 md:pt-8">
-              <div className="flex flex-col pt-4">
-                <div className="flex relative ">
-                  <input
-                    type="text"
-                    required
-                    id="design-login-name"
-                    className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder="Your Name"
-                  />
-                </div>
-              </div>
+              <div className="flex flex-col pt-4"></div>
               <div className="flex flex-col pt-4">
                 <div className="flex relative ">
                   <span className=" inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
@@ -86,7 +76,7 @@ const Registration = () => {
                 </div>
               </div>
               <div className="flex flex-col pt-4 mb-12">
-                <div className="flex relative ">
+                <div className="flex relative mb-4">
                   <span className=" inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
                     <svg
                       width="15"
@@ -104,6 +94,27 @@ const Registration = () => {
                     id="design-login-password"
                     className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                     placeholder="Password"
+                  />
+                </div>
+
+                <div className="flex relative">
+                  <span className=" inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                    <svg
+                      width="15"
+                      height="15"
+                      fill="currentColor"
+                      viewBox="0 0 1792 1792"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"></path>
+                    </svg>
+                  </span>
+                  <input
+                    type="password"
+                    required
+                    id="design-login-password"
+                    className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    placeholder="Confirm Password"
                   />
                 </div>
               </div>
